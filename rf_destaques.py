4adec3194101f82e45573496119222a1d3854fa7
Rafael Dadoorian <30996645+rdpreg@users.noreply.github.com>
2025-12-17 10:16:08 -0300

    Update and rename rf_destques.py to rf_destaques.py

---
+++ rf_destaques.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Teste Streamlit")
+st.write("Se você está vendo isso, o deploy funcionou.")
--- rf_destques.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import pandas as pd
-from datetime import datetime, date
-from io import BytesIO
