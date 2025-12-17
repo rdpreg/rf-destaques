dbb40a45580a89405d06127765ff67b809d010ad
Rafael Dadoorian <30996645+rdpreg@users.noreply.github.com>
2025-12-17 09:48:06 -0300

    Update rf_destques.py

--- rf_destques.py
+++ rf_destques.py
@@ -2,6 +2,3 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, date
 from io import BytesIO
-
-st.set_page_config(page_title="Seleção de Crédito Bancário", layout="wide")
-
